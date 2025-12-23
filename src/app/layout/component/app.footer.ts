69d4a75cfa65202104f208a12a63890bebb701ad
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-12-23 19:51:07 -0300

    tela de admin

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     standalone: true,
     selector: 'app-footer',
-    template: `<div class="layout-footer">
-        Nexus Finance
-    </div>`
+    template: `
+      <div class="layout-footer">
+        © 2025 Nexus Finance. Sistema desenvolvido por Leandro Barreto.
+      </div>
+    `
 })
 export class AppFooter {}
